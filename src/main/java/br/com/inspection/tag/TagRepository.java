666064a4211701ae65b4ac89283f17f26df576d2
Ricardo Corrent <ricardocorrent@hotmail.com>
2019-09-11 15:11:58 -0300

    (2019-09-11) Creating pagination and search (#3)

--- src/main/java/br/com/inspection/tag/TagRepository.java
+++ src/main/java/br/com/inspection/tag/TagRepository.java
@@ -1,10 +1,17 @@
 package br.com.inspection.tag;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TagRepository extends CrudRepository<Tag, UUID> {
+public interface TagRepository extends PagingAndSortingRepository<Tag, UUID> {
+
+    @Query("select t from Tag t where t.title like lower(concat ('%', :title, '%'))")
+    Page<Tag> findTagByTitle(@Param("title") final String title, final Pageable pageable);
 }
