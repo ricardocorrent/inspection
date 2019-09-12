d2d913dfb614a7e77209ba5797fa2e453c1bb907
Ricardo Corrent <ricardocorrent@hotmail.com>
2019-09-12 14:36:22 -0300

    (#154gva) 2019-09-12 trying to insert hateoas on tag

--- src/main/java/br/com/inspection/tag/TagController.java
+++ src/main/java/br/com/inspection/tag/TagController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -13,12 +14,11 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/tag")
+@RequestMapping(value = "/api/tag" , produces = "application/hal+json")
 public class TagController {
 
     private final TagService tagService;
 
-    @Inject
     public TagController(final TagService tagService) {
         this.tagService = tagService;
     }
@@ -32,8 +32,17 @@ public class TagController {
 
     @GetMapping(path = "/{id}", produces = {"application/json", "application/xml"})
     private ResponseEntity<?> getTagById(@PathVariable final UUID id) {
-        TagVO tagVO = tagService.findById(id);
+        TagVO tagVO = new TagVO(){{
+            setKey(id);
+            setTitle("Novo titulo");
+            setCreatedAt(OffsetDateTime.now());
+            setUpdatedAt(OffsetDateTime.now());
+        }};
+        tagVO.add(linkTo(TagController.class).withRel("tag"));
+        tagVO.add(linkTo(methodOn(TagController.class).list()).withRel("memberships"));
         tagVO.add(linkTo(methodOn(TagController.class).getTagById(id)).withSelfRel());
+
+        //tagVO.add(linkTo(methodOn(TagController.class)).withRel("tag"));
         return ResponseEntity.ok(tagVO);
     }
 
