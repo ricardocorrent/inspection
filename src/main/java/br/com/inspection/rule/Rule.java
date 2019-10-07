50c910f2446499506b0b56392188e70a0821b8fd
Ricardo Corrent <ricardocorrent@hotmail.com>
2019-10-07 14:57:52 -0300

    #16xd04 Create Rule Information (#18)

--- src/main/java/br/com/inspection/rule/Rule.java
+++ src/main/java/br/com/inspection/rule/Rule.java
@@ -1,14 +1,14 @@
 package br.com.inspection.rule;
 
+import br.com.inspection.rule.information.RuleInformation;
 import br.com.inspection.server.model.PhysicalBaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter
 @Setter
@@ -25,5 +25,9 @@ public class Rule extends PhysicalBaseEntity {
     @Column(name = "description")
     @Size(max = 150)
     private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "rule_id", nullable = false)
+    private List<RuleInformation> informations;
 
 }
