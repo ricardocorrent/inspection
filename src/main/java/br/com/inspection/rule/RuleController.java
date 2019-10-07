e605852d4fd9d0a3085469cc6494669e864ded11
Ricardo Corrent <ricardocorrent@hotmail.com>
2019-10-07 15:23:48 -0300

    #171rda Creating list all in abstract controller

--- src/main/java/br/com/inspection/rule/RuleController.java
+++ src/main/java/br/com/inspection/rule/RuleController.java
@@ -1,17 +1,9 @@
 package br.com.inspection.rule;
-import br.com.inspection.server.AbstractController;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedResources;
-import org.springframework.hateoas.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
+import br.com.inspection.server.AbstractController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -21,25 +13,13 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RequestMapping(value = "/api/rule")
 public class RuleController extends AbstractController<Rule, RuleVO> {
 
-    @Inject
-    private RuleService ruleService;
-
-    @GetMapping(path = "/rules")
-    public ResponseEntity<PagedResources<Resource<RuleVO>>> list(@RequestParam(value = "page", defaultValue = "0") int page,
-                                                                   @RequestParam(value = "limit", defaultValue = "4") int limit,
-                                                                   @RequestParam(value = "direction", defaultValue = "asc") String direction,
-                                                                   PagedResourcesAssembler<RuleVO> assembler) {
-        final Sort.Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
-        final Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "title"));
-        final Page<RuleVO> listOfRules = ruleService.list(pageable);
-        listOfRules.stream().forEach(this::generateHateoas);
-
-        return new ResponseEntity<>(assembler.toResource(listOfRules), HttpStatus.OK);
-    }
-
     @Override
     public void generateHateoas(final RuleVO ruleVO) {
         ruleVO.add(linkTo(methodOn(RuleController.class).getEntityById(ruleVO.getKey())).withSelfRel());
     }
 
+    @Override
+    protected String getListAllSortProperty() {
+        return "title";
+    }
 }
--- src/main/java/br/com/inspection/server/AbstractController.java
+++ src/main/java/br/com/inspection/server/AbstractController.java
@@ -2,6 +2,13 @@ package br.com.inspection.server;
 
 import br.com.inspection.server.model.BaseModel;
 import br.com.inspection.server.model.BaseVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,6 +52,21 @@ public abstract class AbstractController<T extends BaseModel, Z extends BaseVO> 
                 .body(abstractService.update(z));
     }
 
+    @GetMapping(path = "/list-all")
+    public ResponseEntity<PagedResources<Resource<Z>>> listAll(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                               @RequestParam(value = "limit", defaultValue = "4") int limit,
+                                                               @RequestParam(value = "direction", defaultValue = "asc") String direction,
+                                                               PagedResourcesAssembler<Z> assembler) {
+        final Sort.Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        final Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, getListAllSortProperty()));
+        final Page<Z> listOfEntities = abstractService.list(pageable);
+        listOfEntities.stream().forEach(this::generateHateoas);
+
+        return new ResponseEntity<>(assembler.toResource(listOfEntities), HttpStatus.OK);
+    }
+
     public abstract void generateHateoas(final Z z);
+
+    protected abstract String getListAllSortProperty();
 
 }
