c3f3eca5130b1048dcd62c7e52bf3c3b354e4ebc
Ricardo Corrent <ricardocorrent@hotmail.com>
2019-10-08 14:19:24 -0300

    #171maq Insert target_tag feature

--- src/main/resources/db/migration/V2.0.0__Create_Permission.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `permission` (
-  `id` BINARY(16) NOT NULL,
-  `description` VARCHAR(255) NULL DEFAULT 'null',
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
--- src/main/resources/db/migration/V6.0.0__Create_Tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `rule` (
-  `id` BINARY(16) NOT NULL,
-  `title` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(150) NULL,
-  `created_at` DATETIME NULL,
-  `updated_at` DATETIME NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
