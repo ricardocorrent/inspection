e4db28c24e2554910f000e6983163a2172071842
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-01-30 17:00:17 -0300

    #1d2hta - Getting items from rule using ruleId (#25)

--- src/main/java/br/com/inspection/item/ItemRepository.java
+++ src/main/java/br/com/inspection/item/ItemRepository.java
@@ -1,10 +1,16 @@
 package br.com.inspection.item;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ItemRepository extends PagingAndSortingRepository<Item, UUID> {
+
+    @Query("SELECT i FROM Item i WHERE i.rule.id =:ruleId")
+    List<Item> findByRuleId(@Param("ruleId") UUID ruleId);
 }
