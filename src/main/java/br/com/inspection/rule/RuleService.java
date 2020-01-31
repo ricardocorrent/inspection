7efdb650ed9fcbebf2367015194553c8da2c7ffd
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-01-31 10:50:33 -0300

    1d38tm - Add get all items from rule id into item controller (#27)

--- src/main/java/br/com/inspection/rule/RuleService.java
+++ src/main/java/br/com/inspection/rule/RuleService.java
@@ -1,20 +1,10 @@
 package br.com.inspection.rule;
 
-import br.com.inspection.item.ItemService;
-import br.com.inspection.item.ItemVO;
 import br.com.inspection.server.AbstractService;
-import org.apache.commons.collections.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 public class RuleService extends AbstractService<Rule, RuleVO> {
-
-    @Autowired
-    private ItemService itemService;
 
     @Override
     public Class<RuleVO> getClazz() {
@@ -26,18 +16,4 @@ public class RuleService extends AbstractService<Rule, RuleVO> {
         return Rule.class;
     }
 
-    public RuleItemsVO findItemsByRuleId(final UUID id) {
-        final RuleVO rule = getById(id);
-        return setItemsVOIntoRuleVO(rule);
-    }
-
-    private RuleItemsVO setItemsVOIntoRuleVO(final RuleVO ruleVO) {
-        final List<ItemVO> listItems = itemService.getItemsByRuleId(ruleVO.getKey());
-        final RuleItemsVO ruleItemsVO = new RuleItemsVO();
-        ruleItemsVO.setKey(ruleVO.getKey());
-        if (CollectionUtils.isNotEmpty(listItems)) {
-            ruleItemsVO.setItems(listItems);
-        }
-        return ruleItemsVO;
-    }
 }
