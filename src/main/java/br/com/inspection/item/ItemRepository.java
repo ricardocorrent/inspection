655c97c8d9ed1f2b2246b92b159f635a3ec78c8c
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-02-05 20:31:11 -0300

    #1d5m8c - Adding inspection information (#29)

--- src/main/java/br/com/inspection/item/ItemRepository.java
+++ src/main/java/br/com/inspection/item/ItemRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,4 +14,8 @@ public interface ItemRepository extends PagingAndSortingRepository<Item, UUID> {
 
     @Query("SELECT i FROM Item i WHERE i.rule.id =:ruleId")
     List<Item> findByRuleId(@Param("ruleId") UUID ruleId);
+
+    @Transactional(readOnly = true)
+    Item findByCode(String code);
+
 }
